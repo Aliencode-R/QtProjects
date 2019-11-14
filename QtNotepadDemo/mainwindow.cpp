f2c473db70405b4e2ddf6ab3860034c6aa549af4
Aliencode-R <risharma654@gmail.com>
2019-11-14 18:39:32 +0530

    Notepad updated

--- QtNotepadDemo/mainwindow.cpp
+++ QtNotepadDemo/mainwindow.cpp
@@ -1,11 +1,18 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include<QFile>
+#include<QFileDialog>
+#include<QTextStream>
+#include<QMessageBox>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    // remove spaces from the side of text edit.
+    this->setCentralWidget(ui->textEdit);
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +20,91 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+// triggers for toolbar buttons
+void MainWindow::on_actionNew_triggered()
+{
+    file_path = "";
+    ui->textEdit->setText("");
+}
+
+void MainWindow::on_actionOpen_triggered()
+{
+    QString file_name = QFileDialog::getOpenFileName(this,"Open the file");
+    QFile file(file_name);
+    file_path = file_name; // global variable
+    if(!file.open(QFile::ReadOnly | QFile::Text)) {
+        QMessageBox::warning(this,"file not open","");
+        return;
+    }
+    QTextStream in(&file);
+    QString text = in.readAll();
+    ui->textEdit->setText(text);
+    file.close();
+}
+
+void MainWindow::on_actionSave_triggered()
+{
+
+    QFile file(file_path);
+
+    if(file_path == ""){
+        on_actionSave_as_triggered();
+    }
+
+    if(!file.open(QFile::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this,"file not saved","");
+        return;
+    }
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
+    file.flush();
+    file.close();
+}
+
+void MainWindow::on_actionSave_as_triggered()
+{
+    QString file_name = QFileDialog::getSaveFileName(this,"Open the file");
+    QFile file(file_name);
+    file_path = file_name;
+    if(!file.open(QFile::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this,"file not saved","");
+        return;
+    }
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
+    file.flush();
+    file.close();
+}
+
+void MainWindow::on_actionCut_triggered()
+{
+    ui->textEdit->cut();
+}
+
+void MainWindow::on_actionCopy_triggered()
+{
+     ui->textEdit->copy();
+}
+
+void MainWindow::on_actionPaste_triggered()
+{
+     ui->textEdit->paste();
+}
+
+void MainWindow::on_actionRedo_triggered()
+{
+     ui->textEdit->redo();
+}
+
+void MainWindow::on_actionUndo_triggered()
+{
+     ui->textEdit->undo();
+}
+
+void MainWindow::on_actionAbout_Notepad_triggered()
+{
+    QString text = "Auther : Rishabh Sharma \nDate : 14 November \nVersion : 1.00";
+    QMessageBox::about(this,"About Notepad",text);
+}
